38d0b62c79d18a14ffb9939543f180a441be1d8b
yang_YC <yangyicheng@yangyichengdeMacBook-Air.local>
2022-04-17 20:52:40 +0800

    function update

--- TEST_FUNCTION.cpp
+++ TEST_FUNCTION.cpp
@@ -41,7 +41,7 @@ void ALL_TEST_FUNCTION(double *x, double *f, int dim, int function_num){
     if(function_num >= 1 && function_num <= 10){
         Unimodal_Function(x, f, dim, function_num);
     }
-    else if(function_num >= 11 && function_num <= 21){
+    else if(function_num >= 11 && function_num <= 20){
         Multimodal_Function(x, f, dim, function_num - 10);
     }
     else{
@@ -157,10 +157,9 @@ void Unimodal_Function(double *x, double *f, int dim, int function_num){
 *No.5 Griewank_Function
 *No.6 Schwefel 2.26
 *No.7 LEVY03_Function
-*No.8 LEVY05_Function
-*No.9 STYBLINSKI_TANG_Function
-*No.10 Salomon_Function
-*No.11 Whitley_Function
+*No.8 STYBLINSKI_TANG_Function
+*No.9 Salomon_Function
+*No.10 Whitley_Function
 */
 
 void Multimodal_Range(double *max, double *min, int dim, int function_num){
@@ -174,8 +173,8 @@ void Multimodal_Range(double *max, double *min, int dim, int function_num){
             *min = -32.768;
             break;
         case 3:
-            *max = 5.12;
-            *min = -5.12;
+            *max = M_PI;
+            *min = 0.0;
             break;
         case 4:
             *max = 100.0;
@@ -194,18 +193,14 @@ void Multimodal_Range(double *max, double *min, int dim, int function_num){
             *min = -10.0;
             break;
         case 8:
-            *max = 2.0;
-            *min = -2.0;
-            break;
-        case 9:
             *max = 5.0;
             *min = -5.0;
             break;
-        case 10:
+        case 9:
             *max = 100.0;
             *min = -100.0;
             break;
-        case 11:
+        case 10:
             *max = 10.24;
             *min = -10.24;
             break;
@@ -238,15 +233,12 @@ void Multimodal_Function(double *x, double *f, int dim, int function_num){
             LEVY03_Function(x, f, dim);
             break;
         case 8:
-            LEVY05_Function(x, f, dim);
-            break;
-        case 9:
             STYBLINSKI_TANG_Function(x, f, dim);
             break;
-        case 10:
+        case 9:
             Salomon_Function(x, f, dim);
             break;
-        case 11:
+        case 10:
             Whitley(x, f, dim);
             break;
         default:
@@ -269,19 +261,35 @@ void ACKLEY_Function(double *x, double *f, int DIM){
 }
 
 void RASTRIGIN_Function(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    for(int i = 0; i < DIM; ++i){
+        t1 += (x[i] * x[i]) - (10 * cos(2 * M_PI * x[i]));
+    }
+    f[0] = 10 * DIM + t1;
 }
 
 void ROSENBROCK_Function(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    for(int i = 0; i < DIM - 1; ++i){
+        t1 += 100 * pow(x[i + 1] - x[i] * x[i], 2) + pow(x[i] - 1, 2);
+    }
+    f[0] = t1;
 }
 
 void SPHERE_Function(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    for(int i = 0; i < DIM; ++i){
+        t1 += x[i] * x[i];
+    }
+    f[0] = t1;
 }
 
 void Michalewicz_Function(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    for(int i = 0; i < DIM; ++i){
+        t1 +=  sin(x[i]) * pow(sin((i * x[i] * x[i]) / M_PI), 20);
+    }
+    f[0] = t1;
 }
 
 void Bent_Cigar_Function(double *x, double *f, int DIM){
@@ -293,7 +301,12 @@ void Bent_Cigar_Function(double *x, double *f, int DIM){
 }
 
 void Schaffer_F7_Function(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    for(int i = 0; i < DIM - 1; ++i){
+        double t2 = sqrt(pow(x[i], 2) + pow(x[i + 1], 2));
+        t1 += sqrt(t2) + sqrt(t2) * pow(sin(50.0 * pow(t2, 0.2)), 2);
+    }
+    f[0] = pow(1.0 / (DIM - 1) * t1, 2);
 }
 
 void Zakharov_Function(double *x, double *f, int DIM){
@@ -307,11 +320,21 @@ void Zakharov_Function(double *x, double *f, int DIM){
 }
 
 void Griewank_Function(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    double t2 = 0.0;
+    for(int i = 0; i < DIM; ++i){
+        t1 += pow(x[i], 2) / 4000.0;
+        t2 *= cos(x[i] / sqrt(i));
+    }
+    f[0] = t1 - t2 + 1;
 }
 
 void Schwefel_226_Function(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    for(int i = 0; i < DIM; ++i){
+        t1 += x[i] * sin(sqrt(fabs(x[i])));
+    }
+    f[0] = 418.9829 * DIM - t1;
 }
 
 void SUM_SQUARES_Function(double *x, double *f, int DIM){
@@ -323,23 +346,44 @@ void SUM_SQUARES_Function(double *x, double *f, int DIM){
 }
 
 void LEVY03_Function(double *x, double *f, int DIM){
-
-}
-
-void LEVY05_Function(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    double Wd = 1.0 + ((x[DIM - 1] - 1.0) / 4.0);
+    for(int i = 0; i < DIM - 1; ++i){
+        double Wi = 1.0 + ((x[i] - 1.0) / 4.0);
+        t1 += pow(Wi - 1, 2) * (1.0 + 10 * pow(sin(M_PI * Wi + 1.0), 2)) + pow(Wd - 1, 2) * (1.0 + pow(sin(2 * M_PI * Wd), 2));
+    }
+    double W1 = 1.0 + ((x[0] - 1.0) / 4.0);
+    f[0] = pow(sin(W1 * M_PI), 2) + t1;
 }
 
 void STYBLINSKI_TANG_Function(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    for(int i = 0; i < DIM; ++i){
+        t1 += pow(x[i], 4) - 16 * pow(x[i], 2) + 5 * x[i];
+    }
+    f[0] = 0.5 * t1;
 }
 
 void Salomon_Function(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    for(int i = 0; i < DIM; ++i){
+        t1 += x[i] * x[i];
+    }
+    f[0] =  -cos(2 * M_PI * sqrt(t1)) + 0.1 * sqrt(t1) + 1.0;
 }
 
 void Whitley(double *x, double *f, int DIM){
-
+    double t1 = 0.0;
+    for(int i = 0; i < DIM; ++i){
+        double t2 = 0.0;
+        for(int j = 0; j < DIM; ++j){
+            double a = pow(100.0 * pow(pow(x[i], 2) - x[j], 2) + pow(1.0 - x[j], 2), 2) / 4000.0;
+            double b = cos(100.0 * pow(pow(x[i], 2) - x[j], 2) + pow(1.0 - x[j], 2));
+            t2 += a - b + 1.0;
+        }
+        t1 += t2;
+    }
+    f[0] = t1;
 }
 
 void Brown_Function(double *x, double *f, int DIM){
